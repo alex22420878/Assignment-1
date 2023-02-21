e76aec0dbc660b4b989357477c77b52ba6039527
[22420878@life.hkbu.edu.hk] <[22420878@life.hkbu.edu.hk]>
2023-02-21 20:21:45 +0800

    updates for output info

--- clean.py
+++ clean.py
@@ -1,16 +1,22 @@
 import pandas as pd
 
-df1= pd.read_csv('respondent_contact.csv')
-df2= pd.read_csv('respondent_other.csv')
+df1= pd.read_csv ('respondent_contact.csv')
+df2= pd.read_csv ('respondent_other.csv')
 
 #merge
-merge_file = pd.merge (df1, df2,
-                        left_on='respondent_id',right_on='id',
-                        how= 'outer')
+merge_file = pd.merge(df1, df2,
+                      left_on='respondent_id', right_on='id',
+                      how='outer')
 
 #drop
-df3=merge_file.drop (['id'],axis=1)
+df3=merge_file. drop(['id'],axis=1)
 df4=merge_file.dropna(axis=0)
-output_fite=df4[df4["job"].str.contains("insurance | Insurance")==False]
+output_file=df4[df4['job'].str.contains('insurance|Insurance')==False]
+
 #export
-output_fite.to_csv('output_file.csv',index=False)
+output_file.to_csv('output_file.csv',index=False)
+
+#step3
+print(output_file)
+print("Output file shape:")
+print(output_file.shape)
